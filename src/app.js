da44c368d927187ff3070c9eae2965068e423d6f
Rogelio Alatorre <rojo.perfeccion@gmail.com>
2017-02-21 01:28:29 -0600

    Fetch the posts info

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@
 let Post = {
   findAll(){
     return new Promise( (resolve, reject) => {
-      resolve("ok posts!");
+      let uri = "http://localhost:3000/posts";
+      let request = new XMLHttpRequest();
+
+      request.open("GET", uri, true);
+
+      request.onload = () => {
+        if(request.status >= 200 && request.status < 400) {
+          resolve( JSON.parse(request.response) );
+        }
+      };
+
+      request.onerror = () => {
+        reject( new Error("Houston we have an error on the API") );
+      };
+
+      request.send();
     });
   }
 }
@@ -13,4 +28,8 @@ let ui = {
   }
 }
 
-Post.findAll().then(ui.renderPosts);
+Post.findAll()
+  .then(ui.renderPosts)
+  .catch( (error) => {
+    console.log(error)
+  });
